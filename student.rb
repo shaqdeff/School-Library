b81c4512e7f0062f5fe3b070e750257ae04c4393
shaqdeff <shaquillendunda@gmail.com>
2022-11-12 00:34:20 +0300

    Set classrom for student

--- student.rb
+++ student.rb
@@ -2,14 +2,22 @@ require './person'
 
 # extends person class
 class Student < Person
+   attr_accessor :classroom, :type
   # extends parent's constructor by adding @classroom and a parameter for it
   def initialize(classroom, age, name = 'Unknown', parent_permission: true)
     super(age, name, parent_permission)
     @classroom = classroom
+    @type = 'Student'
   end
 
   # create playh_hooky method
   def play_hooky
     '¯(ツ)/¯'
+  end
+end
+
+def add_to_classroom(classroom)
+    @classroom = classroom
+    classroom.student << self unless classroom.student.include?(self)
   end
 end
