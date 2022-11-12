835a70468be83d3ecb68d493db3650da37e90059
shaqdeff <shaquillendunda@gmail.com>
2022-11-12 04:30:55 +0300

    Fix bug

--- app.rb
+++ app.rb
@@ -4,37 +4,37 @@ require_relative 'student'
 require_relative 'teacher'
 require_relative 'person'
 
-# Create a app.rb file that will serve as your console app entry-point. It should have methods that do the following:
-# List all books.
-# List all people.
-# Create a person (teacher or student, not a plain Person).
-# Create a book.
-# Create a rental.
-# List all rentals for a given person id.
-
- def list_books
-  Book.all.each do |book|
-    puts "Title: \"#{book.title}\", Author: #{book.author}"
+class App
+  def initialize
+    @books = []
+    @people = []
+    @rentals = []
   end
 
-  puts 'No books added' if Book.all.empty?
-end
+  def list_books
+    @books.each do |book|
+      puts "Title: \"#{book.title}\", Author: #{book.author}"
+    end
+
+    puts 'No books added' if @books.empty?
+  end
 
   def list_people
-    Person.all.each do |person|
+    @people.each do |person|
       puts "[#{person.class}] Name: #{person.name}, ID: #{person.id}, Age: #{person.age}"
     end
 
-    puts 'No people found' if Person.all.empty?
+    puts 'No people found' if @people.empty?
   end
 
   def create_person
     print 'Do you want to create a student (1) or a teacher (2)? [Input the number]: '
     person_type = gets.chomp
 
-    if person_type == '1'
+    case person_type
+    when '1'
       create_student
-    elsif person_type == '2'
+    when '2'
       create_teacher
     else
       puts 'Invalid option'
@@ -45,14 +45,15 @@ end
   def create_student
     print 'Age: '
     age = gets.chomp
-    
+
     print 'Name: '
     name = gets.chomp
-    
+
     print 'Has parent permission? [Y/N]: '
     parent_permission = gets.chomp.downcase == 'y'
 
     student = Student.new(age, name, parent_permission: parent_permission)
+    @people << student
     puts 'Person created successfully'
     student
   end
@@ -68,6 +69,7 @@ end
     specialization = gets.chomp
 
     teacher = Teacher.new(age, name, specialization)
+    @people << teacher
     puts 'Person created successfully'
     teacher
   end
@@ -80,20 +82,21 @@ end
     author = gets.chomp
 
     book = Book.new(title, author)
+    @books << book
     puts 'Book created successfully'
     book
   end
 
   def create_rental
     puts 'Select a book from the following list by number'
-    Book.all.each_with_index do |book, index|
+    @books.each_with_index do |book, index|
       puts "#{index}) Title: \"#{book.title}\", Author: #{book.author}"
     end
 
     book_index = gets.chomp.to_i
 
     puts 'Select a person from the following list by number (not id)'
-    Person.all.each_with_index do |person, index|
+    @people.each_with_index do |person, index|
       puts "#{index}) [#{person.class}] Name: #{person.name}, ID: #{person.id}, Age: #{person.age}"
     end
 
@@ -102,7 +105,8 @@ end
     print 'Date: '
     date = gets.chomp
 
-    rental = Rental.new(date, Book.all[book_index], Person.all[person_index])
+    rental = Rental.new(date, @books[book_index], @people[person_index])
+    @rentals << rental
     puts 'Rental created successfully'
     rental
   end
@@ -111,9 +115,10 @@ end
     print 'ID of person: '
     id = gets.chomp.to_i
 
-    rentals = Rental.all.select { |rental| rental.person.id == id }
+    rentals = @rentals.select { |rental| rental.person.id == id }
     puts 'Rentals:'
     rentals.each do |rental|
       puts "Date: #{rental.date}, Book \"#{rental.book.title}\" by #{rental.book.author}"
     end
   end
+end
--- main.rb
+++ main.rb
@@ -1,7 +1,12 @@
 require './app'
 
+#  define the entry point
 def main
+  #  create a new app instance
+  app = App.new
+  #  print welcome message
   puts 'Welcome to School Library App!'
+  #  print options
   puts 'Please choose an option by entering a number:'
   puts '1 - List all books'
   puts '2 - List all people'
@@ -10,30 +15,30 @@ def main
   puts '5 - Create a rental'
   puts '6 - List all rentals for a given person id'
   puts '7 - Exit'
-
+  #  get user input
   option = gets.chomp
-
+  #  call the method that corresponds to the option
   case option
   when '1'
-    list_books
+    app.list_books
   when '2'
-    list_people
+    app.list_people
   when '3'
-    create_person
+    app.create_person
   when '4'
-    create_book
+    app.create_book
   when '5'
-    create_rental
+    app.create_rental
   when '6'
-    list_rentals_for_person_id
+    app.list_rentals_for_person_id
   when '7'
     puts 'Thank you for using this app!'
-    exit 0
+    return
   else
     puts 'That is not a valid option'
   end
-  
+  #  repeat the process
   main
 end
 
-  main
+main
